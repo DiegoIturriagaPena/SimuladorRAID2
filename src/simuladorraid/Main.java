572ed0d0615c071cf7d3adf91b26035d741dbdfc
ArielBass <arcornejo15@alumnos.utalca.cl>
2018-08-10 20:24:24 -0400

    Ventana Principal
    Se creo ventana Principal

--- src/simuladorraid/Main.java
+++ src/simuladorraid/Main.java
@@ -5,17 +5,28 @@
  */
 package simuladorraid;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Diego Iturriaga
  */
-public class Main {
-
+public class Main extends Application {
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.showAndWait();
+        System.exit(0);//To change body of generated methods, choose Tools | Templates.
     }
     
 }
---
+++ src/simuladorraid/VentanaPrincipal.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladorraid;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ariel
+ */
+public class VentanaPrincipal extends Stage implements EventHandler<Event> {
+    private BorderPane root;
+    private Button Abrir;
+    private Button raid0;
+    private Button raid1;
+    private Button raid2;
+    private Button raid3;
+    private Button raid4;
+    private Button raid5;
+    private Button raid6;
+    ArrayList<String> archivoCargado;
+    public VentanaPrincipal() {
+        this.archivoCargado = new ArrayList<>();
+        this.root = new BorderPane();
+        Scene scene = new Scene (root,960,542);
+        this.setScene(scene);
+        this.doTop();
+        this.doCenter();
+        this.setTitle("Raid Simulato: arcade triple omega mata pulgas y garrapatas ");
+    }
+    
+    
+    @Override
+    public void handle(Event event) {
+        if(event.getSource() == this.Abrir && event instanceof ActionEvent)
+        {
+           FileChooser fileChooser = new FileChooser();
+           fileChooser.setTitle("Abrir Archivo"); 
+           File file = fileChooser.showOpenDialog(this); 
+           if(file!=null)
+           {
+               this.activarBotones();
+               String nombreDelArhivo = file.getParent() +"\\"+file.getName();
+               FileReader in = null;
+               try {
+                    in = new FileReader(file);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+               BufferedReader fi = new BufferedReader(in);
+               Object[] datos = fi.lines().toArray();
+               for(int i=0;i<datos.length;i++)
+               {
+                   String aux = (String)datos[i];
+                   this.archivoCargado.add(aux);
+               }
+               
+               
+           }
+        }
+    }
+
+    private void doTop() {
+       ToolBar botonera = new ToolBar();
+       this.Abrir = new Button("Abrir");
+       this.Abrir.addEventHandler(EventType.ROOT, this);
+       botonera.getItems().add(Abrir);
+       VBox contenedor = new VBox();
+       contenedor.getChildren().add(botonera);
+       this.root.setTop(contenedor);
+    }
+
+    private void doCenter() {
+        VBox botones = new VBox();
+        botones.setSpacing(10);
+        botones.setPadding(new Insets(5,5,5,10));
+        this.raid0 = new Button("Raid 0");
+        this.raid0.setDisable(true);
+        this.raid1 = new Button("Raid 1");
+        this.raid1.setDisable(true);
+        this.raid2 = new Button("Raid 2");
+        this.raid2.setDisable(true);
+        this.raid3 = new Button("Raid 3");
+        this.raid3.setDisable(true);
+        this.raid4 = new Button("Raid 4");
+        this.raid4.setDisable(true);
+        this.raid5 = new Button("Raid 5");
+        this.raid5.setDisable(true);
+        this.raid6 = new Button("Raid 6");
+        this.raid6.setDisable(true);
+        botones.getChildren().addAll(this.raid0,this.raid1,this.raid2,this.raid3,this.raid4,this.raid5,this.raid6);
+        this.root.setCenter(botones);
+        
+        
+        
+    }
+    
+    public void activarBotones()
+    {
+        this.raid0.setDisable(false);
+        this.raid1.setDisable(false);
+        this.raid2.setDisable(false);
+        this.raid3.setDisable(false);
+        this.raid4.setDisable(false);
+        this.raid5.setDisable(false);
+        this.raid6.setDisable(false);
+    }
+    
+}
